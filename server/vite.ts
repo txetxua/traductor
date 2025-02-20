98285a15402ec12e6dfa3e67681af17bfbc4decf
txetxua <txe.allue@gmail.com>
2025-02-20 18:07:54 +0100

    Guardando cambios antes de hacer pull

--- server/vite.ts
+++ server/vite.ts
@@ -3,8 +3,6 @@ import fs from "fs";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { createServer as createViteServer, createLogger } from "vite";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 import { type Server } from "http";
 import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
